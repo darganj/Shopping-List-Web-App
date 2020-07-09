a4426b59aeca1ad8b8fafa944b53b565506ee17f
gaidosj <56010019+gaidosj@users.noreply.github.com>
2020-07-09 10:54:32 -0700

    created first draft website template

---
+++ app.js
@@ -0,0 +1,95 @@
+var express = require('express');
+// var mysql = require('./dbcon.js');
+var app = express();
+
+// set handlebars
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+// set server info
+app.disable('x-powered-by');
+app.set('port', 5001);
+
+// set using info
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// make url encoded info in query string usable
+var queryParams = function(req, res, next) {
+  res.parts = req.query;
+  next();
+};
+app.use(queryParams);
+
+
+var fakeData = 
+            {"name":"bob", 
+                "lists":
+                [{"listName":"April", 
+                "listItems":
+                    {"item1":"apple",
+                    "item2":"pear",
+                    "item3":"peach"
+                    }
+                },
+                {"listName":"May", 
+                "listItems":
+                    {"item1":"beetle",
+                    "item2":"ant",
+                    "item3":"ladybug"
+                    }
+                },
+                {"listName":"June", 
+                "listItems":
+                    {"item1":"meat",
+                    "item2":"meatier",
+                    "item3":"meatiest"
+                    }
+                }]
+                
+            };
+
+
+app.get('/',function(req,res,next){
+  res.render('home');
+});
+
+app.get('/about',function(req,res,next){
+  res.render('about');
+});
+
+app.get('/login',function(req,res,next){
+  res.render('login');
+});
+
+app.post('/login',function(req,res,next){
+  res.render('shoppinglist',{fakeData:fakeData});
+});
+
+app.get('/register',function(req,res,next){
+  res.render('register');
+});
+
+app.post('/register',function(req,res,next){
+    res.render('shoppinglist',{fakeData:fakeData});
+});
+
+// 404 error route
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+  
+// 500 server error route
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+  
+// listen command
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://' + process.env.HOSTNAME + ':' + app.get('port') + '; press Ctrl-C to terminate.');
+});

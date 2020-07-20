add1c56af83dadbc7f3cc36ed0901f9eb3e68b9f
adpangelinan <adrian.pang92@outlook.com>
2020-07-19 22:19:59 -0500

    Edit connection

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ var MySQLStore = require('express-mysql-session')(session);
 
 if(process.env.JAWSDB_URL){
     var connection = mysql.createConnection(process.env.JAWSDB_URL);
-
-
 }else{
   var connection = mysql.createConnection({
     host            : 'localhost',

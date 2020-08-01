e2db0efd703b90114c24547301b395c89efc010f
adpangelinan <adrian.pang92@outlook.com>
2020-08-01 11:43:28 -0500

    implementing shoppinglistovw routes

--- app.js
+++ app.js
@@ -247,9 +247,10 @@ app.use(bodyParser.json());
 //New Functions for each section
 app.set('connection', connection);
 app.use('/userlanding', require('./userlanding.js')); //Routes to user landing page
+app.use('/shoppinglistovw', require('./shoppinglistovw.js')); //Routes to View groups of shopping lists
 /*
 app.use('/adminlanding', require('./adminlanding.js')); //Routes to admin landing page
-app.use('/shoppinglistovw', require('./shoppinglistovw.js')); //Routes to View groups of shopping lists
+
 app.use('/shoppinglist', require('./shoppinglist.js')); //Routes to view an individual shopping list
 app.use('/edit-list', require('./edit-list.js')); //Routes to edit a shopping list
 app.use('/login', require('./login.js')); //Routes for logging in

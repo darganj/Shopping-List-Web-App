580e289183be59e45fc98c73ebc698dff60d962c
shukieliosu <59763257+shukieliosu@users.noreply.github.com>
2020-08-07 20:35:36 +0800

    - Moved database credentials into separate file called “dbcon.js” with an exported connection object that can be referenced in all .js files inside the app’s home folder.   - Moved POST route to ‘/shoppinglist’ into the “shoppinglist.js” file from the “app.js” file and referenced the connection object from inside “shoppinglist.js” mentioned above for any sql queries sent to the database

--- shoppinglist.js
+++ shoppinglist.js
@@ -50,6 +50,98 @@ router.get('/', ensureLoggedIn.ensureLoggedIn('/login'), function (req, res, nex
 
 });
 
+router.post('/', ensureLoggedIn.ensureLoggedIn('/login'), function (req, res, next) {
+    res.locals.login = req.isAuthenticated();
+
+    // marking an item
+    if (req.body.Unchecked) { // include "markItem" value in submit element to indicate option 1
+        console.log("check item reached");
+        var {listID, listOfItems} = req.body; // required front-end args: listID, itemID, quantity
+        connection.query('UPDATE List_of_Items SET markStatus=1 WHERE listID=? AND listOfItems=?', [listID, listOfItems], function (err, result) {
+            if (err) {
+                next(err);
+                return;
+            };
+        });
+
+        // fetch & re-render updated list of items
+        var context = {};
+        var connection = req.app.get('connection');
+        var listName = req.query.nameList
+        var callbackCount = 0;
+
+
+        getItems(res, listName, connection, context, complete);
+        function complete() {
+            console.log('made it to callback');
+            callbackCount++;
+            if (callbackCount >= 1) {
+                res.render('shoppinglist', { context: context.listitems });
+            }
+        }
+    }
+
+    // unmarking an item
+    else if (req.body.Checked) {
+        console.log("uncheck item reached");
+        var { listID, listOfItems } = req.body; // required front-end args: listID, itemID, quantity
+        connection.query('UPDATE List_of_Items SET markStatus=0 WHERE listID=? AND listOfItems=?', [listID, listOfItems], function (err, result) {
+            if (err) {
+                next(err);
+                return;
+            };
+        });
+
+        // fetch & re-render updated list of items
+        var context = {};
+        var connection = req.app.get('connection');
+        var listName = req.query.nameList
+        var callbackCount = 0;
+
+
+        getItems(res, listName, connection, context, complete);
+        function complete() {
+            console.log('made it to callback');
+            callbackCount++;
+            if (callbackCount >= 1) {
+                res.render('shoppinglist', { context: context.listitems });
+            }
+        }
+
+    }
+
+    else {
+
+        var { listID, itemID, quantity } = req.body;
+        connection.query('INSERT INTO List_of_Items (`listID`, `itemID`, `quantity`, `markStatus`) VALUES (?, ?, ?, 0)', [listID, itemID, quantity], function (err, result) {
+            if (err) {
+                next(err);
+                return;
+            };
+        });
+
+        console.log(req.body);
+
+        //Render the shopping list view with correct items
+
+        var context = {};
+        var connection = req.app.get('connection');
+        var listName = req.query.nameList
+        var callbackCount = 0;
+
+
+        getItems(res, listName, connection, context, complete);
+        function complete() {
+            console.log('made it to callback');
+            callbackCount++;
+            if (callbackCount >= 1) {
+                res.render('shoppinglist', { context: context.listitems });
+            }
+        }
+
+    };
+
+});
 
 
 

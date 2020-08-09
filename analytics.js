1816211d2f058fa3d618f90bdf603e9efdbf9741
zgershuny <gershunz@oregonstate.edu>
2020-08-08 23:01:29 -0400

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -11,49 +11,21 @@ var express_enforces_ssl = require('express-enforces-ssl');
 var ensureLoggedIn = require('connect-ensure-login');
 var router = express.Router(); //Creates the router middleware variable
 
-
-// Get UserName
-/*
-function getUserName(res, userID, connection, context, complete) { //if any info required for query, need it here as well
-
-    var query = "SELECT * FROM Users WHERE Users.userID = ?";
-    console.log("Implementing the query...");
-    connection.query(query, userID, function (err, results, fields) {
-        if (err) {
-            console.log("Danger, Danger Will Robinson");
-            next(err);
-            return;
-
-        }
-        console.log("Querying completed...");
-        context.userlists = results; //Can't just put results in context, will cause problems
-        console.log("Stored results in userlists...");
-        complete(); //Routes back to your router function so data can be displayed
-    });
-
-}
-*/
-
-/*Sample Get Route, Some imporant notes:
- * Must be router.path, not app.path
- * Whatever 'use' route you put in app.js to get here, that is already accounted for
- * For example if i put in app.js app.use'/routepath' to get here then this route will work for www.website/routepath
- * If I put /routepath in this router then the route to get there would be www.website/routepath/routepath*/
-
-
 // Display page
 router.get('/', ensureLoggedIn.ensureLoggedIn('/login'), function (req, res, next) { //Include any data required for query as well
     res.locals.login = req.isAuthenticated();
     res.locals.user = req.user;
     var context = {};
-    userID = res.locals.user.userName;
-    userName = res.locals.user.userID;
+    context.userID = res.locals.user.userName;
+    context.userName = res.locals.user.userID;
   
-    var connection = req.app.get('connection'); //You must put this in every route, this pulls database connection into route
+    // var connection = req.app.get('connection'); //You must put this in every route, this pulls database connection into route
     console.log(1);
     
+    res.render('analytics', {context: context});
+    
     //var query = "SELECT * FROM Users WHERE Users.userID = ?";
-    connection.query("SELECT * FROM Users WHERE Users.userID=4", userID, function(err, result){
+    /* connection.query("SELECT * FROM Users WHERE Users.userID=4", userID, function(err, result){
         if(err){
             console.log("Query Error");
             next(err);
@@ -64,6 +36,8 @@ router.get('/', ensureLoggedIn.ensureLoggedIn('/login'), function (req, res, nex
         res.render('analytics', context);
         console.log(3);
     });
+    */
+    
     // getUserName(res, userID, connection, context, complete); //Pulls data into context, Include any data required for query as well
     
    

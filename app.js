c76815b532ae5a0876cca463fbba9f40218e4b10
zgershuny <gershunz@oregonstate.edu>
2020-08-09 19:18:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -21,46 +21,6 @@ app.use(helmet.referrerPolicy({ policy: 'no-referrer' }));
 app.enable('trust proxy');
 app.use(express_enforces_ssl());
 
-// if(process.env.JAWSDB_URL){
-//     var connection = mysql.createConnection(process.env.JAWSDB_URL);
-// }else{
-//   console.log("Error: You need to configure your local database parameters");
-//   // var connection = mysql.createConnection({
-//   //   host            : 'mwgmw3rs78pvwk4e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-//   //   user            : 'oumwqrox1u690kyf',
-//   //   password        : 'dp0jxu78jfqeqry1',
-//   //   database        : 'rtfqihn2cy96dcmm'
-//   // });
-// }
-// var options = {
-// 	// Host name for database connection:
-// 	host: 'localhost',
-// 	// Port number for database connection:
-// 	port: 3306,
-// 	// Database user:
-// 	user: 'flj1jzapfhtiwjjo',
-// 	// Password for the above database user:
-// 	password: 'ztb0cti8o5648gsw',
-// 	// Database name:
-// 	database: 'zv3sbfb4eij4y18x',
-// 	// Whether or not to automatically check for and clear expired sessions:
-// 	clearExpired: true,
-// 	// How frequently expired sessions will be cleared; milliseconds:
-// 	checkExpirationInterval: 900000,
-// 	// The maximum age of a valid session; milliseconds:
-// 	expiration: 86400000,
-// 	// Whether or not to create the sessions database table, if one does not already exist:
-// 	createDatabaseTable: true,
-// 	// Number of connections when creating a connection pool:
-// 	connectionLimit: 1,
-// 	// Whether or not to end the database connection when the store is closed.
-// 	// The default value of this option depends on whether or not a connection was passed to the constructor.
-// 	// If a connection object is passed to the constructor, the default value for this option is false.
-// 	endConnectionOnClose: true
-// };
-
-// var sessionStore = new MySQLStore(connection);
-
 var sessionStore = new MySQLStore({
   checkExpirationInterval: 900000,// How frequently expired sessions will be cleared; milliseconds.
   expiration: 86400000,// The maximum age of a valid session; milliseconds.
@@ -200,13 +160,7 @@ passport.use('local-login', new LocalStrategy(
 
             return done(null, false);
           }
-            // req.session.loggedin = true;
-            // req.session.username = username;
-            // res.redirect('shoppinglist');
         }
-        
-        // res.render('shoppinglist', { context: context });
-    // });
   
       })
     }
@@ -538,33 +492,6 @@ app.post('/shoppinglistovw/mergelists', /*ensureLoggedIn.ensureLoggedIn('/login'
     });
 
 });
-
-
-
-// route to update the item in the list
-/*app.get('/edit-list',function(req,res,next){
-  var context = {};
-
-  // sql placeholder variable
-  var getShoppingList = "SELECT Users.userName, Lists.nameList, List_of_Items.quantity, Items.itemName " +
-  "FROM Users " +
-    "LEFT JOIN Lists ON Lists.userID = Users.userID " +
-    "LEFT JOIN List_of_Items ON List_of_Items.listID = Lists.listID " +
-    "LEFT JOIN Items ON List_of_Items.itemID = Items.itemID " +
-  "WHERE Users.userID=? AND Lists.listID=?;";
-
-  // execute the sql to render and display the shopping list
-  connection.query(getShoppingList, function(err, result){
-    if (err){
-      console.log(1);
-      next(err);
-      return;
-    }
-    context.list = result;
-    res.render('edit-list');
-  });
-});
-*/
 
 app.get('/edit-list', ensureLoggedIn.ensureLoggedIn('/login'), function (req, res, next) {
   res.locals.login = req.isAuthenticated();

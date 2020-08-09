37b7e678cdc40a14184fe6de9c0f5128683a5c22
zgershuny <gershunz@oregonstate.edu>
2020-08-09 01:28:06 -0400

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -23,6 +23,80 @@ router.get('/', ensureLoggedIn.ensureLoggedIn('/login'), function (req, res, nex
     // var connection = req.app.get('connection'); //You must put this in every route, this pulls database connection into route
     console.log(1);
     
+  
+    if(res.locals.user.isAdmin != 1){
+      res.redirect('userlanding');
+    }else{
+      res.render('analytics', { context: context });
+    }
+ 
+    // console.log(4)
+/*
+    if (req.query.ascending){
+        var popAscOrder = "SELECT Items.itemName, COUNT(List_of_Items.itemID) AS counted " +
+            "FROM List_of_Items " +
+            "JOIN Items ON List_of_Items.itemID=Items.itemID " +
+            "GROUP BY itemName " +
+            "ORDER BY COUNT(List_of_Items.itemID) DESC";
+        
+        connection.query(popAscOrder, function(err, popAscResults){
+            if(err){
+                console.log("ERROR: Ascending Order Query");
+                next(err);
+                return;
+            };
+            context.order = popAscResults;
+            console.log("Ascending Order Querying Completed");
+            res.render('analytics', context);
+        });
+    }
+    else if (req.query.descending){
+        var popDescOrder = "SELECT Items.itemName, COUNT(List_of_Items.itemID) AS counted " +
+            "FROM List_of_Items " +
+            "JOIN Items ON List_of_Items.itemID=Items.itemID " +
+            "GROUP BY itemName " +
+            "ORDER BY counted ASC";
+        
+        connection.query(popDescOrder, function(err, popDescResults){
+            if(err){
+                console.log("ERROR: Descending Order Query");
+                next(err);
+                return;
+            };
+            context.order = popDescResults;
+            console.log("Descending Order Querying Completed");
+            res.render('analytics', context);
+        });
+    }
+    else
+    {
+        var defaultOrder = "SELECT Items.itemName, COUNT(List_of_Items.itemID) AS counted " +
+            "FROM List_of_Items " +
+            "JOIN Items ON List_of_Items.itemID=Items.itemID " +
+            "GROUP BY itemName " +
+            "ORDER BY itemName ASC";
+        
+        connection.query(defaultOrder, function(err, defaultOrderResults){
+            if(err){
+                console.log("ERROR: Default Order Query");
+                next(err);
+                return;
+            };
+            context.order = defaultOrderResults;
+            console.log("Default Order Querying Completed");
+            res.render('analytics', context);
+        });
+    }
+    */
+    
+});
+        
+router.get('/', function (req, res, next) { //Include any data required for query as well
+    var context = {};
+  
+    // var connection = req.app.get('connection'); //You must put this in every route, this pulls database connection into route
+    console.log(1);
+    
     /*
     if(res.locals.user.isAdmin != 1){
       res.redirect('userlanding');
@@ -89,6 +163,5 @@ router.get('/', ensureLoggedIn.ensureLoggedIn('/login'), function (req, res, nex
     }
     
 });
-        
     
 module.exports = router;
